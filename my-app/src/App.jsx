8e1126a761ad5bc8d96777a74d5e1ad679572c2b
kabilanpersonal <kabilan0904@gmail.com>
2025-08-07 07:41:51 +0530

    Restructuring

---
+++ my-app/src/App.jsx
@@ -0,0 +1,21 @@
+import BMI from './BMI'
+import Accordion from './Accordion'
+import CarouselSlick from './CarouselSlick'
+import CountdownTimer from './CountdownTimer'
+import Dropdown from './Dropdown'
+import Resignation from './Resignation'
+function App() {
+ 
+  return (
+    <>
+      {/* <BMI/> */}
+      {/* <Accordion/> */}
+      {/* <CarouselSlick/> */}
+      {/* <CountdownTimer /> */}
+      {/* <Dropdown /> */}
+      <Resignation />
+    </>
+  );
+}
+
+export default App
